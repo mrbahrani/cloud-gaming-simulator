66ccba086615ea528cfabc3b8c24e62514f01f87
Ali Bahrani <ali.bahrani94521036@gmail.com>
2021-12-15 18:28:27 -0800

    length, type added

--- packet.py
+++ packet.py
@@ -1,12 +1,18 @@
 class Packet:
-    def __init__(self, identity, player, game, timeout, task):
+    def __init__(self, identity, player, game, l, timeout, task, typ='res'):
         self.id = identity
         self.player = player
         self.game = game
         self.timout = timeout
         self.task = task
+        self.length = l
+        self.type = typ
         self.history = dict()
         self.path = None
 
     def set_path(self, path):
         self.path = path
+
+    def next_hop(self):
+        pass
+
