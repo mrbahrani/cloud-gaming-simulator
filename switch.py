64a2c72c967d897bc509d5cd1a8dfbb4c84a36ee
Ali Bahrani <ali.bahrani94521036@gmail.com>
2021-12-16 15:09:58 -0800

    added sending packet time

--- switch.py
+++ switch.py
@@ -8,11 +8,12 @@ class Switch(SimulationEntity):
     def parse_message(self, msg):
         pass
 
-    def __init__(self, identity=0, max_size=math.inf):
+    def __init__(self, identity=0, max_size=math.inf, sending_time=0.01):
         super().__init__()
         self.id = identity
         self.packet_queue = Queue()
         self.max_size = max_size
+        self.sending_packet_time = sending_time
 
     def receive(self, packet):
         self.packet_queue.put(packet)
