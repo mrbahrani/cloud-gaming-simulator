9c9326a704e191383ce8cb1cac5ccc5ce0929970
Ali Bahrani <ali.bahrani94521036@gmail.com>
2021-12-23 13:30:07 -0800

    constructor modified

--- absgraph.py
+++ absgraph.py
@@ -1,8 +1,9 @@
 from abc import ABC
 
+
 class AbstractGraph(ABC):
     def __init__(self):
-        raise NotImplementedError
+        pass
 
     def add_node(self, item):
         raise NotImplementedError
