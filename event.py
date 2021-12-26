bfadb5152167adb9d6aeec7b8c7099f78107e9bd
Ali Bahrani <ali.bahrani94521036@gmail.com>
2021-12-26 13:03:42 -0800

    add device to event

--- event.py
+++ event.py
@@ -1,7 +1,7 @@
 class Event:
-    def __init__(self, code,  start, end, packet):
+    def __init__(self, code,  start, end, packet, device=None):
         self.code = code
         self.start = 0
         self.end = 0
         self.packet = packet
-        self.device = None
+        self.device = device
