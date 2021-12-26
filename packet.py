3b34e1f73751f3a7a360cec5795fa2e3a6f47a16
Ali Bahrani <ali.bahrani94521036@gmail.com>
2021-12-26 13:03:04 -0800

    handle hops in the way!

--- packet.py
+++ packet.py
@@ -9,10 +9,16 @@ class Packet:
         self.type = typ
         self.history = dict()
         self.path = None
+        self.current_next_hop = 0
 
     def set_path(self, path):
         self.path = path
 
-    def next_hop(self):
-        pass
+    def go_to_next_hop(self):
+        self.current_next_hop += 1
+        return self.path[self.current_next_hop]
+
+    def peek_next_hop(self):
+        return self.path[self.current_next_hop+1]
+
 
